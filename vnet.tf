1deccf2e5c5088cb4f128f58c4443e656dfdb952
NansyWGates <nansygates@gmail.com>
2021-02-10 02:27:40 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/NansyGates-training/network/azurerm"
   version = "3.0.1"
-  # insert required variables here
-  resource_group_name = nswgates 
+
+  resource_group_name = "nswgates"
 }
