b7a48b642250d3e7dc8f4cedc8b424a5062fb874
NansyWGates <nansygates@gmail.com>
2021-02-10 02:06:00 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,11 +2,8 @@ module "network" {
   source  = "app.terraform.io/NansyGates-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  # resource_group_name = "nswgates"
+  "nswgates"
   
-  variable resource_group_name {
-    description = "The name of an existing resource group to be imported."
-    type = string
-  }
+  
 
 }
